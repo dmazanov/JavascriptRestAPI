5fb932b8116dbd3f9caced60b269f7667fdf7c25
Denys <d.mazanov@global4net.com>
2022-09-03 14:24:46 +0200

    read data from a file

--- index.js
+++ index.js
@@ -5,16 +5,18 @@ let pieRepo = require('./repos/pieRepo');
 
 // Use th express Router object
 let router = express.Router();
-let pies = pieRepo.get();
-
 
 // Create GET to return a list of all pies
 router.get('/', function(req, res, next){
-    res.status(200).json({
-        "status": 200,
-        "statusText": "OK",
-        "message": "All pies retrieved.",
-        "data": pies
+    pieRepo.get(function(data) {
+        res.status(200).json({
+            "status": 200,
+            "statusText": "OK",
+            "message": "All pies retrieved.",
+            "data": data
+        });
+    }, function(err){
+        next(err);
     });
 });
 
